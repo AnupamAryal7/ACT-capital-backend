703217b6e96c6553888a03d37c2cbed54d1888c2
AnupamAryal7 <anmaryal41@gmail.com>
2025-07-12 23:52:21 +0545

    modified init.ph

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from src.db.main import initdb
 
 # add lifespan event
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("the server is running...")
+    await initdb()
     yield
     print("the server is stopped.")
 
